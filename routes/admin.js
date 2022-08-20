5de47aeda43a605229ae9324e5a6899852ab30ea
Djurdjina Lopicic <djlopicic16@raf.rs>
2022-08-20 16:57:28 +0200

    .

--- routes/admin.js
+++ routes/admin.js
@@ -39,7 +39,7 @@ function authTokena(req, res, next){
     const token = cookies['token'];
 
 
-    if(token == null) return res.redirect('/admin/login');
+  //  if(token == null) return res.redirect('/admin/login');
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
   //      if(err) return res.redirect('/admin/login');
